34c2ec6be96f00674833baa9558f5dea64919c6d
Chetan Meshram <chmeshra@visa.com>
2017-05-08 12:24:02 -0700

    build script gets expected Node version

--- jakefile.js
+++ jakefile.js
@@ -1,9 +1,22 @@
 (function() {
     "use strict";
 
+    var packageJson = require("./package.json");
+    var expectedVERSION = packageJson.engines.node;
+
     desc("Default build");
-    task("default", function() {
+    task("default", ["version"], function() {
      console.log("\n\nBuild OK!!");
+    });
+
+    desc("Check Node version");
+    task("version", function() {
+        console.log("Checking Node version: .");
+        let actualVersion = process.version;
+
+        if(actualVersion !== expectedVERSION) {
+            fail(`Node versions do not match. Expected: ${expectedVERSION}, but actual versios is: ${actualVersion}.`);
+        }
     });
 
 }());
